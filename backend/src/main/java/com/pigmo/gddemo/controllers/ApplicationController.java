6ffb1f27ca81c6880833cac3f18139fd9b37a9c3
陈旭 <chenxu@tongbaninfo.com>
2020-02-19 22:44:56 +0800

    生成PDF

--- backend/src/main/java/com/pigmo/gddemo/controllers/ApplicationController.java
+++ backend/src/main/java/com/pigmo/gddemo/controllers/ApplicationController.java
@@ -6,6 +6,7 @@ import com.pigmo.gddemo.services.xckancha.scbiaodan.ScbiaodanService;
 import com.pigmo.gddemo.utils.NormalResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- backend/src/main/java/com/pigmo/gddemo/services/xckancha/scbiaodan/ScbiaodanService.java
+++ backend/src/main/java/com/pigmo/gddemo/services/xckancha/scbiaodan/ScbiaodanService.java
@@ -3,7 +3,11 @@ package com.pigmo.gddemo.services.xckancha.scbiaodan;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.*;
+import com.pigmo.gddemo.dto.ApplyInfoDto;
+import com.pigmo.gddemo.services.xckancha.hedui.SqxinxiService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -12,35 +16,111 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Service
 @Slf4j
 public class ScbiaodanService {
 
-    public void testDoc(){
+    @Value("${my.outpath}")
+    private String path;
+
+    @Autowired
+    private SqxinxiService sqxinxiService;
+
+
+    public void testDoc() {
+        String filename = "/" + LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")) + Instant.ofEpochMilli(4) + ".pdf";
         Resource resource = new ClassPathResource("/file/ywbd.pdf");
-        Document document = new Document();
+        InputStream is = null;
+        FileOutputStream out = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        PdfReader reader = null;
         try {
-            InputStream is = resource.getInputStream();
-            final FileOutputStream out = new FileOutputStream("/Users/jchen/Documents/file/test.pdf");
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            PdfReader reader = new PdfReader(is);
+            is = resource.getInputStream();
+            out = new FileOutputStream(path + filename);
+            reader = new PdfReader(is);
             PdfStamper stamper = new PdfStamper(reader, bos);
             AcroFields form = stamper.getAcroFields();
 
-            form.setField("u1","武藏顺");
+            BaseFont bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+            ArrayList<BaseFont> fontList = new ArrayList<>();
+            fontList.add(bf);
+            form.setSubstitutionFonts(fontList);
+
+            Map<String, String> map = getData(1l);
+            fillData(form, map);
             stamper.setFormFlattening(true);
             stamper.close();
-            PdfCopy cp = new PdfCopy(document, out);
-            document.open();
-            PdfImportedPage ip = cp.getImportedPage(new PdfReader(bos.toByteArray()), 1);
-            cp.addPage(ip);
-            document.close();
 
+            out.write(bos.toByteArray());
 
-//            PdfWriter.getInstance(document,new FileOutputStream("/file"));
+            out.flush();
+            out.close();
+            bos.close();
         } catch (IOException | DocumentException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void fillData(AcroFields form, Map map) throws IOException, DocumentException {
+        for (Object key : map.keySet()) {
+            form.setField(key.toString(), map.get(key).toString());
+        }
+    }
+
+    private Map<String, String> getData(Long id) {
+        Map<String, String> map = new HashMap();
+        ApplyInfoDto info = sqxinxiService.getApplicationInfo(id);
+        if (info != null) {
+            map.put("u1", Instant.ofEpochMilli(8).toString());
+            map.put("u2", info.getAppNum());
+            map.put("u3", info.getClientName());
+            map.put("u4", "测试联系人");
+            map.put("u5", "18392839201");
+            map.put("u6", "成都市");
+            map.put("u7", "双流区");
+            map.put("u8", "东升街道");
+            map.put("u9", "东升社区");
+            map.put("u10", "华府大道");
+            map.put("u11", "438");
+            map.put("u12", "测试备注");
+            map.put("u13", String.valueOf(LocalDate.now().getYear()));
+            map.put("u14", String.valueOf(LocalDate.now().getMonth()));
+            map.put("u15", String.valueOf(LocalDate.now().getDayOfMonth()));
+            map.put("u16", info.getPowerUseType().toString());
+            map.put("u17", "√");
+            map.put("u18", "□");
+            map.put("u20", info.getIndustryType().toString());
+            map.put("u21", "√");
+            map.put("u22", "□");
+            map.put("u24", info.getOriginalVolume());
+            map.put("u25", info.getOriginalVolume());
+            map.put("u26", "√");
+            map.put("u27", "□");
+            map.put("u28", "□");
+            map.put("u29", "□");
+            map.put("u30", "□");
+            map.put("u32", "测试接入点");
+            map.put("u33", "√");
+            map.put("u34", "□");
+            map.put("u35", "测试组成");
+            map.put("u36", "√");
+            map.put("u37", "□");
+            map.put("u38", "□");
+            map.put("u39", "√");
+            map.put("u40", "□");
+            map.put("u41", "□");
+            map.put("u44", "√");
+            map.put("u45", "130");
+            map.put("u46", "□");
+            map.put("u47", "测试计量方式");
+            map.put("u48", "测试安装位置");
+            map.put("u49", "测试备注");
+        }
+        return map;
     }
 }
